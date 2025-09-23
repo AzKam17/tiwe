c451bc218118043b03dd733ec7a260e93aa762e4
AmadouAzizKAMAGATE <aziz.kamagate@djamo.io>
2025-09-23 11:18:52 +0000

    feat: list products on home page

--- app/src/Repository/ProductRepository.php
+++ app/src/Repository/ProductRepository.php
@@ -16,6 +16,11 @@ class ProductRepository extends ServiceEntityRepository
         parent::__construct($registry, Product::class);
     }
 
+    public function getTrending()
+    {
+        return $this->findAll();
+    }
+
 //    /**
 //     * @return Product[] Returns an array of Product objects
 //     */
