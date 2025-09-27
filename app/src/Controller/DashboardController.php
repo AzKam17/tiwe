03c3bb1d9b32052adf68315bba8eb3bed89fa3a2
AmadouAzizKAMAGATE <aziz.kamagate@djamo.io>
2025-09-27 12:47:25 +0000

    feat: recharge account form

--- app/src/Controller/DashboardController.php
+++ app/src/Controller/DashboardController.php
@@ -2,18 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[IsGranted('ROLE_USER')]
 final class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard_home')]
-    public function index(): Response
+    public function index(#[CurrentUser] User $user): Response
     {
-        return $this->render('dashboard/home.html.twig');
+        return $this->render('dashboard/home.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/recharge', name: 'app_dashboard_recharge')]
+    public function recharge(#[CurrentUser] User $user, Request $request): Response
+    {
+        $amount = $request->request->get('amount');
+        $accountHolder = $request->request->get('account_holder');
+        $transactionProof = $request->files->get('transaction_proof');
+
+        dump($amount, $accountHolder, $transactionProof);
+        return $this->redirectToRoute('app_dashboard_home', [], Response::HTTP_SEE_OTHER);
     }
 }
