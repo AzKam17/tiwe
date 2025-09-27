b5c8bcecf8b9a930f4ec7fdd984c2ec3b88da4f0
AmadouAzizKAMAGATE <aziz.kamagate@djamo.io>
2025-09-27 16:08:00 +0000

    feat: update product with modal

--- app/src/Controller/Products/AuthProductController.php
+++ app/src/Controller/Products/AuthProductController.php
@@ -7,6 +7,7 @@ namespace App\Controller\Products;
 use App\Entity\Product;
 use App\Entity\User;
 use App\Form\NewProductType;
+use App\Repository\ProductCategoryRepository;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,12 +22,12 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class AuthProductController extends AbstractController
 {
     #[Route('/', name: 'app_auth_products_home')]
-    public function products(#[CurrentUser] User $user,ProductRepository $repository): Response
+    public function products(#[CurrentUser] User $user, ProductRepository $repository, ProductCategoryRepository $productCategoryRepository): Response
     {
         $products = $repository->findBy(['createdBy' => $user], []);
-        dump($products);
         return $this->render('dashboard/products.html.twig', [
             'products' => $products,
+            'categories' => $productCategoryRepository->findAll(),
         ]);
     }
 
@@ -39,7 +40,6 @@ class AuthProductController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            dump("dada");
             $product = $form->getData();
             $em->persist($product);
             $em->flush();
@@ -50,5 +50,38 @@ class AuthProductController extends AbstractController
         return $this->render('dashboard/products/new.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/update/{id}', name: 'app_auth_products_update')]
+    public function update(
+        Product $product,
+        #[CurrentUser] User $user,
+        Request $request,
+        EntityManagerInterface $em,
+        ProductCategoryRepository $categoryRepository
+    ): Response
+    {
+        $form = $request->request->all();
+
+        $categoryId = intval($form['category']);
+        if ($categoryId !== $product->getCategory()->getId()) {
+            $category = $categoryRepository->find($categoryId);
+            if ($category) {
+                $product->setCategory($category);
+            }
+        }
+
+        $product
+            ->setTitle($form['title'])
+            ->setPrice(intval($form['price']))
+            ->setStock(intval($form['stock']))
+            ->setDescription($form['description']);
+
+        $em->persist($product);
+        $em->flush();
+
+        $this->addFlash('success', 'Inventaire mise à jour avec succès.');
+
+        return $this->redirectToRoute('app_auth_products_home', []);
     }
 }
